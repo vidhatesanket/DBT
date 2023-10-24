8adc5271b06d474777a6ab927989d3918ca3bc2a
Vidhatesanket18 <sanketvidhate7745@gmail.com>
2023-10-24 19:23:06 +0530

    24-10 7:22

--- plsql assignment programs/trigger 1/trigger5.sql
+++ plsql assignment programs/trigger 1/trigger5.sql
@@ -1,10 +1,10 @@
 
 drop trigger if exists trigger5;
 delimiter $
-create trigger trigger5 before insert on student for each row
+create trigger trigger5 before insert on student1 for each row
 BEGIN
 	declare result varchar(30);
-	if dayname(now()='Saturday') then
+	if dayname(now()='Friday') then
 		signal sqlstate '45000'
 		set message_text="Today is Sunday, Data will not be inserted!";
 	end if;
